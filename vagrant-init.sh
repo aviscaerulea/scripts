a776c3374d0efd1dc8b8ec77edffed7a161048bb
aviscaerulea <aviscaerulea@users.noreply.github.com>
2022-05-03 07:37:02 +0900

    Update vagrant-init.sh

--- vagrant-init.sh
+++ vagrant-init.sh
@@ -1,27 +1,34 @@
 #!/bin/bash -u
+printf "install insecure pub key of vagrant official\n"
 readonly KEY_FILE="/home/vagrant/.ssh/authorized_keys"
 install -o vagrant -g vagrant -m 0700 -d $(dirname $KEY_FILE)
 install -o vagrant -g vagrant -m 0600 /dev/null $KEY_FILE
 curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o $KEY_FILE
 
+printf "change grub config\n"
 sed -re 's/^(GRUB_TIMEOUT)=.+/\1=0/' -i".bkup" /etc/default/grub
 diff -U0 /etc/default/grub{.bkup,}
 
+printf "disable SELinux\n"
 setenforce 0
 sed -re 's/^(SELINUX)=.+/\1=disabled/' -i".bkup" /etc/selinux/config
 diff -U0 /etc/selinux/config{.bkup,}
 
+printf "change sshd config\n"
 sed -re 's/^#?(UseDNS) .+/\1 no/' \
     -re 's/^#?(GSSAPIAuthentication) .+/\1 no/' \
     -i".bkup" /etc/ssh/sshd_config
 diff -U0 /etc/ssh/sshd_config{.bkup,}
 
-tee /etc/sudoers.d/vagrant <<EOL > /dev/null
+printf "create sudoers.d for vagrant\n"
+tee /etc/sudoers.d/vagrant <<_EOL_ > /dev/null
 Defaults:vagrant !requiretty
 vagrant ALL=(ALL) NOPASSWD:ALL
-EOL
+_EOL_
 
-tee /etc/sudoers.d/root <<EOL > /dev/null
+printf "create sudoers.d for root\n"
+tee /etc/sudoers.d/root <<_EOL_ > /dev/null
 Defaults:root !requiretty
-EOL
+_EOL_
+
 sudo chmod 0640 /etc/sudoers.d/*
