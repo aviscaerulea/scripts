c8fbb3b0bb657e5fc223890fc4706c66e06e27fb
aviscaerulea <aviscaerulea@users.noreply.github.com>
2022-05-03 07:22:21 +0900

    Update vagrant-inseckey.sh

--- vagrant-inseckey.sh
+++ vagrant-inseckey.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -u
 readonly KEY_FILE="/home/vagrant/.ssh/authorized_keys"
 install -o vagrant -g vagrant -m 0700 -d $(dirname $KEY_FILE)
-curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o $KEY_FILE
-chown vagrant:vagrant $KEY_FILE
-chmod 0600 $KEY_FILE
+install -o vagrant -g vagrant -m 0600 <(curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub) $KEY_FILE
+# curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o $KEY_FILE
+# chown vagrant:vagrant $KEY_FILE
+# chmod 0600 $KEY_FILE
