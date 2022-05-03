9acc4387e7334aebeff6c029f9fb6ffd7f183e7a
aviscaerulea <aviscaerulea@users.noreply.github.com>
2022-05-04 05:22:33 +0900

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -eu
 sudo yum -y install yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum -y install docker-ce
+sudo yum -y install docker-ce docker-compose-plugin
+sudo ln -sf /usr/libexec/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
 sudo systemctl enable docker
-sudo systemctl start docker
+sudo systemctl restart docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
