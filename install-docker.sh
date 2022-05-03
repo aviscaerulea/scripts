e60b60242ea78adee478064f1fc590483aa3788a
aviscaerulea <aviscaerulea@users.noreply.github.com>
2022-05-03 09:20:58 +0900

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -eu
 sudo yum -y install yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum-config-manager --enable docker-ce-edge
 sudo yum -y install docker-ce
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo gpasswd -a $USER docker
 
 cat <<_EOL_ >> $HOME/.bashrc
-alias d="docker"
+alias d="sudo docker"
 _EOL_
 source $HOME/.bashrc
