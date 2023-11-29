32526ff4362f967b473f623cdc968521243edc41
aviscaerulea <aviscaerulea@users.noreply.github.com>
2023-11-30 00:10:09 +0900

    Update vagrant-init.sh

--- vagrant-init.sh
+++ vagrant-init.sh
@@ -5,6 +5,8 @@ install -o vagrant -g vagrant -m 0700 -d $(dirname $KEY_FILE)
 install -o vagrant -g vagrant -m 0600 /dev/null $KEY_FILE
 curl https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o $KEY_FILE
 
+# rm -f /boot/grub2/grubenv
+# grub2-editenv create
 sed -re 's/^(GRUB_TIMEOUT)=.+/\1=0/' -re 's/^(GRUB_DEFAULT)=.+/\1=0/' -i".bkup" /etc/default/grub
 diff -U0 /etc/default/grub{.bkup,}
 grub2-mkconfig -o /boot/grub2/grub.cfg
